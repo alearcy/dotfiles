475b99a21ec864a6952a76a744fa1e930548bb2c
alearcy <arcidiaco.a@gmail.com>
2025-08-23 11:37:04 +0200

    add nvidia support

--- configuration.nix
+++ configuration.nix
@@ -48,7 +48,8 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = true;
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
