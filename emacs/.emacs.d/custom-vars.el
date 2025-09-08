ee224077e353bbc11eacd2f10e420acd934cb1a1
alearcy <arcidiaco.a@gmail.com>
2025-09-08 23:20:33 +0200

    add emacs gitignore

---
+++ emacs/.emacs.d/custom-vars.el
@@ -0,0 +1,16 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0f1341c0096825b1e5d8f2ed90996025a0d013a0978677956a9e61408fcd2c77"
+	 default))
+ '(warning-suppress-types '((flymake flymake))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -935,3 +935,4 @@
 
 (provide 'init)
 ;;; init.el ends here
+(put 'dired-find-alternate-file 'disabled nil)
